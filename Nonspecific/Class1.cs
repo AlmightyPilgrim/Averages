a6e19021da2939e03e10c02d1feb58500aa11617
Bret Hayes <bret_hayes@comcast.net>
2019-02-26 08:34:10 -0500

    Changing doubles to int

--- Nonspecific/Class1.cs
+++ Nonspecific/Class1.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FirstSum;
 
 namespace Nonspecific
 {
     public class Average
     {
-        public double Grade(double track)
+        public int Grade(int track)
         {
-            double grade = 0;
-            double val = 0;
+            int grade = 0;
+            int val = 0;
             track = 0;
-            double sum = 0;
+            int sum = 0;
             int checker = 1;
 
             while (checker == 1)
             {
                 sum = sum + val;
                 Console.WriteLine("Input scores");
-                val = double.Parse(Console.ReadLine());
+                val = int.Parse(Console.ReadLine());
                 if ((val >= 0) && (val <= 100))
                 {
                 }
@@ -35,27 +36,9 @@ namespace Nonspecific
                 checker = int.Parse(Console.ReadLine());
             }
 
-            grade = Math.Round(sum / track);
-            if ((grade <= 90) && (grade >= 100))
-            {
-                Console.WriteLine($"Overall grade is an {grade}, which is an 'A'");
-            }
-            if ((grade <= 80) && (grade >= 89))
-            {
-                Console.WriteLine($"Overall grade is an {grade}, which is an 'D'");
-            }
-            if ((grade <= 60) && (grade >= 79))
-            {
-                Console.WriteLine($"Overall grade is an {grade}, which is an 'C'");
-            }
-            if ((grade <= 50) && (grade >= 69))
-            {
-                Console.WriteLine($"Overall grade is an {grade}, which is an 'D'");
-            }
-            else
-            {
-                Console.WriteLine($"Overall grade is an {grade}, which is an 'F'");
-            }
+            grade = sum / track;
+            SumFirst gradeSum = new SumFirst();
+            gradeSum.TotalGrade(grade);
             return grade;
         }
     }
